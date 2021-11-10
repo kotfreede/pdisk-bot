84e9a870099f3df9ec9896226a5bee1d707bc004
KOT BOTS <88032123+kotfreede@users.noreply.github.com>
2021-11-10 15:34:31 +0530

    Update mainfuc.py

--- plugins/mainfuc.py
+++ plugins/mainfuc.py
@@ -35,7 +35,7 @@ async def _human_time_duration(seconds):
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client,message):
 	insert(int(message.chat.id))
-	await message.reply_text("Hello iam Pdisk Uploader Bot\nMade with love by @mrlokaman",reply_to_message_id = message.message_id,reply_markup=InlineKeyboardMarkup([ [ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], [InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]  ) 
+	await message.reply_text("Hello iam Pdisk Uploader Bot\nMade with love by @KOT_BOTS",reply_to_message_id = message.message_id,reply_markup=InlineKeyboardMarkup([ [ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/KOT_REPORS") ], [InlineKeyboardButton("Subscribe 🧐", url="https://t.me/KOT_BOTS") ]  ]  ) 
         )
 	
 @Client.on_message(filters.command("ping"))
